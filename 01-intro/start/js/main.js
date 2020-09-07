85767188c0f26e07ebe471adb98145e547f89dd7
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-07 20:15:12 +0200

    set a simple parallax effect with GSAP

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -20,6 +20,22 @@ $(document).ready(function(){
   .setPin('#intro', {pushFollowers: false})
   .addTo(controller);
 
+
+  //  parallax scene
+
+  const parallaxTl = new TimelineMax();
+  parallaxTl
+    .from('.content-wrapper', 0.4, {autoAlpha: 0, ease: Power0.easeNone}, 0.4)
+    .from('.bcg', 2, {y: '-30%', ease: Power0.easeNone}, 0);
+    
+  const slideParallaxScene = new ScrollMagic.Scene({
+    triggerElement: '.bcg-parallax',
+    triggerHook: 1,
+    duration: '100%'
+  })
+  .setTween(parallaxTl)
+  .addTo(controller);
+
   //  loop through each .project element
   $('.project').each(function() {
 
