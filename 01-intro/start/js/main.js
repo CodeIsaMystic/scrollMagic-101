a030a772d64dc372e7b27ad9ff19b841067c9c00
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-07 18:01:02 +0200

    switching for a loop

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -3,48 +3,28 @@ $(document).ready(function(){
   //  Init scrollMagic
   const controller = new ScrollMagic.Controller();
 
-  //  Build a scene
-  var ourScene = new ScrollMagic.Scene({
-    triggerElement: '#project01 img',
-    duration: '90%',
-    triggerHook: 0.9
-  })
-  .setClassToggle('#project01', 'fade-in') // add class to the project
-  .addIndicators({
-    name: 'fade scene',
-    colorTrigger: 'black',
-    colorStart: '#75C695',
-    colorEnd: 'pink'
-  }) // require a plugin file
-  .addTo(controller); 
 
-  //  Build a second scene
-  var ourScene2 = new ScrollMagic.Scene({
-    triggerElement: '#project02 img',
-    duration: '90%',
-    triggerHook: 0.9
-  })
-  .setClassToggle('#project02', 'fade-in') // add class to the project
-  .addIndicators({
-    name: 'fade scene 2',
-    colorTrigger: 'black',
-    colorStart: '#75C695',
-    colorEnd: 'pink'
-  }) // need to add the plugin file
-  .addTo(controller); 
+  //  loop through each .project element
+  $('.project').each(function() {
 
-  //  Build a third scene
-  var ourScene3 = new ScrollMagic.Scene({
-    triggerElement: '#project03 img',
-    triggerHook: 0.9
-  })
-  .setClassToggle('#project03', 'fade-in') // add class to the project
-  .addIndicators({
-    name: 'fade scene 3',
-    colorTrigger: 'black',
-    colorStart: '#75C695'
-  }) // need to add the plugin file
-  .addTo(controller); 
+    // look what "this" return
+    console.log(this);
+
+    //  Build a scene
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0], // changing the target element to "this" keyword and target the children(img)
+      triggerHook: 0.9,
+      //duration: '90%'
+    })
+    .setClassToggle(this, 'fade-in') // add class to the project // changing the target element to "this" keyword
+    .addIndicators({
+      name: 'fade scene',
+      colorTrigger: 'black',
+      colorStart: '#75C695',
+      colorEnd: 'pink'
+    }) // require a plugin file
+    .addTo(controller); 
+  });
 
 });
 
