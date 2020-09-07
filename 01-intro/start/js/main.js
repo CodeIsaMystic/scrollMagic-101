d87aa1eb399720d8c4fe175fb48661c8958281e2
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-07 17:48:20 +0200

    duplicating scene (:

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -15,6 +15,34 @@ $(document).ready(function(){
     colorTrigger: 'black',
     colorStart: '#75C695',
     colorEnd: 'pink'
+  }) // require a plugin file
+  .addTo(controller); 
+
+  //  Build a second scene
+  var ourScene2 = new ScrollMagic.Scene({
+    triggerElement: '#project02 img',
+    duration: '90%',
+    triggerHook: 0.9
+  })
+  .setClassToggle('#project02', 'fade-in') // add class to the project
+  .addIndicators({
+    name: 'fade scene 2',
+    colorTrigger: 'black',
+    colorStart: '#75C695',
+    colorEnd: 'pink'
+  }) // need to add the plugin file
+  .addTo(controller); 
+
+  //  Build a third scene
+  var ourScene3 = new ScrollMagic.Scene({
+    triggerElement: '#project03 img',
+    triggerHook: 0.9
+  })
+  .setClassToggle('#project03', 'fade-in') // add class to the project
+  .addIndicators({
+    name: 'fade scene 3',
+    colorTrigger: 'black',
+    colorStart: '#75C695'
   }) // need to add the plugin file
   .addTo(controller); 
 
